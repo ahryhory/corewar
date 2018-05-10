27b7dd9a5ff4b322d3536694f396747810a7006d
Yevhenii BOHUSEVYCH <ybohusev@e3r5p6.unit.ua>
2018-05-10 16:20:12 +0300

    merge parse and write_byte parts

--- asm_src/asm.c
+++ asm_src/asm.c
@@ -15,12 +15,7 @@
 int			main(int argc, char **argv)
 {
 	t_data	data;
-<<<<<<< HEAD
 	data = parse(argv[1]);
-	// write_byte(data);
-=======
-	// data = parse(argv[1]);
 	write_byte(data, argv[1]);
->>>>>>> 5da0e859215e8b2ad54b58a5010c9800f40755be
 	return (0);
 }

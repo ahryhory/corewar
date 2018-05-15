909b6ebb607967723410049fc0cfc74318a059ce
Andrii HRYHORYSHYN <ahryhory@e3r5p9.unit.ua>
2018-05-15 14:52:02 +0300

    add to vm command functions

--- includes/vm.h
+++ includes/vm.h
@@ -18,4 +18,21 @@
 # include <fcntl.h>
 # include <stdio.h>
 
+void	do_add(t_memory *memory, int index, t_proc *proc);
+void	do_aff(t_memory *memory, int index, t_proc *proc);
+void	do_and(t_memory *memory, int index, int *n, t_proc *proc);
+void	do_fork(t_memory *memory, int index, t_proc *proc);
+void	do_ld(t_memory *memory, int index, int *n, t_proc *proc);
+void	do_ldi(t_memory *memory, int index, int *n, t_proc *proc);
+void	do_lfork(t_memory *memory, int index, t_proc *proc);
+void	do_live(t_memory *memory, int index, t_proc *proc);
+void	do_lld(t_memory *memory, int index, int *n, t_proc *proc);
+void	do_lldi(t_memory *memory, int index, int *n, t_proc *proc);
+void	do_or(t_memory *memory, int index, int *n, t_proc *proc);
+void	do_st(t_memory *memory, int index, int *n, t_proc *proc);
+void	do_sti(t_memory *memory, int index, int *n, t_proc *proc);
+void	do_sub(t_memory *memory, int index, t_proc *proc);
+void	do_xor(t_memory *memory, int index, int *n, t_proc *proc);
+void	do_zjmp(t_memory *memory, int index, t_proc *proc);
+
 #endif

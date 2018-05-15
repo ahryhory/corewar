7f4cbb579f22c5e611140d498176d20da12a7f6d
Ivan SELETSKYI <iseletsk@e3r5p8.unit.ua>
2018-05-15 15:44:28 +0300

    s

--- vm_src/vm_hendl_proc.c
+++ vm_src/vm_hendl_proc.c
@@ -6,7 +6,7 @@
 /*   By: iseletsk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/15 13:09:30 by iseletsk          #+#    #+#             */
-/*   Updated: 2018/05/15 13:10:23 by iseletsk         ###   ########.fr       */
+/*   Updated: 2018/05/15 15:42:50 by iseletsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,18 @@
 int		vm_hendl_proc(t_proc *proc)
 {
 	int		i;
+	t_proc	*b_proc;
 
 	i = 0;
+	b_proc = proc;
 	while (proc)
 	{
+		if (!proc->cycl_live)
+		{
+			proc = proc->next;
+			vm_del_proc(b_proc, i);
+			continue;
+		}
 		i++;
 		vm_hendl_byte(proc);
 		proc = proc->next;

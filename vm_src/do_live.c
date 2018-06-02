828fcfda791d4868ba944c518e9b969bf501ae39
Ivan SELETSKYI <iseletsk@e3r3p6.unit.ua>
2018-06-02 15:07:41 +0300

    s

--- vm_src/do_live.c
+++ vm_src/do_live.c
@@ -6,30 +6,44 @@
 /*   By: ahryhory <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/15 14:33:49 by ahryhory          #+#    #+#             */
-/*   Updated: 2018/06/02 12:09:50 by iseletsk         ###   ########.fr       */
+/*   Updated: 2018/06/02 15:04:56 by iseletsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
 
-void	do_live(t_con *con, int index, t_proc *proc)
+static void	s_init(int *nbr, int index, t_con *con)
 {
-	int		nbr[4];
-	int		i;
-
-	con->live++;
-	if (proc->live <= 1)
-		proc->live++;
 	nbr[0] = con->mem[get_index(index, 1)].byte;
 	nbr[1] = con->mem[get_index(index, 2)].byte;
 	nbr[2] = con->mem[get_index(index, 3)].byte;
 	nbr[3] = con->mem[get_index(index, 4)].byte;
-	i = -1;
-	while (++i < 4)
+}
+
+void	do_live(t_con *con, int index, t_proc *proc)
+{
+	int		nbr[4];
+	int		i;
+	t_chemp	*chemp;
+
+	chemp = con->chemp->next;
+	con->live++;
+	if (proc->live <= 1)
+		proc->live++;
+	s_init(nbr, index, con);
+	while (chemp)
 	{
-		if (nbr[i] != con->mem[index].chemp->nbr[i])
+		i = -1;
+		while (++i < 4)
+		{
+			if (nbr[i] != chemp->nbr[i])
+				break ;
+			if (i <= 2)
+				continue;
+			chemp->cycl_live = con->cycl + 1;
+			chemp->live_icp++;
 			return ;
+		}
+		chemp = chemp->next;
 	}
-	con->mem[index].chemp->cycl_live = con->cycl + 1;
-	con->mem[index].chemp->live_icp++;
 }
